9c023a193d66a1206353c8ef77106bc21dddc627
Securety <124460207+bysecurety@users.noreply.github.com>
2023-04-03 02:07:46 -0400

    Update pingsweep-v02-0422-debian.sh

--- source/pingsweep-v02-0422-debian.sh
+++ source/pingsweep-v02-0422-debian.sh
@@ -6,7 +6,7 @@ CACHE_FILE_NAME="pingsweep-$(date +"%FT%H%M")"
 #Variable declaration for SCAN RESULTS folder
 
 #Ping sweeper main folder search on the system
-PS_DIR="$(find / -type d -name PingSweeper-0.2.0-alpha 2>/dev/null)"
+PS_DIR="$(find / -type d -name PingSweeper-main 2>/dev/null)"
 
 #The PATH to the newly created Scan results subfolder
 newScanResultsDirPATH="$PS_DIR/scanresults/scan-$(date +"%FT%H%M")"
